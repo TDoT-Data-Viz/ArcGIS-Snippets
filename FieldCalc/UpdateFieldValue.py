20989774727a983d9449c540e642a46e5d596ee5
Jared Cleghorn <jaredcleghorn@gmail.com>
2020-01-16 08:48:00 -0600

    added 'FieldCalc' folder with init snippet

---
+++ FieldCalc/UpdateFieldValue.py
@@ -0,0 +1,11 @@
+def update_value(value):
+    if value == 1:
+        return "ONE"
+    elif value == 2:
+        return "TWO"
+    elif value == 3:
+        return "THREE"
+    else:
+        return value
+
+update_value(!reference_field!)
